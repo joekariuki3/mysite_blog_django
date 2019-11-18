ca75af2109fe122b60b9dc4cc1f838b2da7f1ab1
joekariuki3 <joekariuki3@gmail.com>
2019-11-18 20:42:07 +0300

    added static in settings.py

--- mysite_blog/settings.py
+++ mysite_blog/settings.py
@@ -143,3 +143,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     ...
 )
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
