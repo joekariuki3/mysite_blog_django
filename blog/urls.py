8bf2d22b62109dda1beef3b9f4cebd29fbc4d538
joekariuki3 <joekariuki3@gmail.com>
2019-11-26 13:59:02 +0300

    removed unwanted files

--- blog/urls.py
+++ blog/urls.py
@@ -56,8 +56,4 @@ urlpatterns = [
     # 127.0.0.1:8000/profile ==> local
     # mydjangoblog.com/profile ==> online
     path('profile/', views.userProfile, name='profile'),
-
-    # 127.0.0.1:8000/landing ==> local
-    # mydjangoblog.com/landing ==> online
-    path('landing_page/', views.landing_page, name='landing_page'),
 ]
