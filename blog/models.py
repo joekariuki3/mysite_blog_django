823465281f18e69f306ab9e9286f3ecae3f6599d
joekariuki3 <joekariuki3@gmail.com>
2024-05-14 18:13:21 +0300

    added image field with a default of null

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=60)
     text = models.TextField()
+    image = models.ImageField(upload_to='images', null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     edited_date = models.DateTimeField(blank=True, null=True)
