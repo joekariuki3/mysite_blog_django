0c7bd5b8746f86f8f1ea78d09bff4804b8ebb6d8
joekariuki3 <joekariuki3@gmail.com>
2024-12-05 15:56:17 +0300

    make sure filename exists of filename does not have ./ infont of it add it make sure file_name(bash script) has execution permissions

--- update_app.py
+++ update_app.py
@@ -1,9 +1,14 @@
 import subprocess
 import sys
+import os
+import stat
+
+# Define the error message for incorrect usage
+error_message = "Usage: update_app.py BASH_SCRIPT_FILE_NAME.sh BRANCH_NAME REPOSITORY_NAME"
 
 # Check if we have the correct number of arguments
 if len(sys.argv) != 4:
-    print("Usage: python run_bash_script.py FILE_NAME BRANCH_NAME REPOSITORY_NAME")
+    print(error_message)
     sys.exit(1)
 
 
@@ -11,6 +16,24 @@ if len(sys.argv) != 4:
 file_name = sys.argv[1]  # Bash script file
 branch_name = sys.argv[2]  # Branch name
 repository_name = sys.argv[3]  # Repository name
+
+# Check if the file exists
+if not os.path.isfile(file_name):
+    print(f"Error: The file '{file_name}' does not exist.")
+    print(error_message)
+    sys.exit(1)
+
+# Prepend './' to the file name if it's in the current directory
+if not file_name.startswith('./'):
+    file_name = f'./{file_name}'
+
+# Check if the file has execution permission
+file_stat = os.stat(file_name)
+if not bool(file_stat.st_mode & stat.S_IXUSR):
+    print(
+        f"'{file_name}' does not have execution rights. Granting execution permission.")
+    # Grant user execute (u+x) permission
+    os.chmod(file_name, file_stat.st_mode | stat.S_IXUSR)
 
 # Run the bash script using subprocess
 try:
