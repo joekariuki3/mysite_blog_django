e1678cdebe4118f099ecd6eb0ce88206f5fe40da
joekariuki3 <joekariuki3@gmail.com>
2024-12-05 15:30:19 +0300

    pass arguments using python file all the way to bash script to be run

--- update_app.py
+++ update_app.py
@@ -1,15 +1,24 @@
 import subprocess
+import sys
 
-# Define the path to the bash script
-bash_script = './updateApplication.sh'
+# Check if we have the correct number of arguments
+if len(sys.argv) != 4:
+    print("Usage: python run_bash_script.py FILE_NAME BRANCH_NAME REPOSITORY_NAME")
+    sys.exit(1)
+
+
+# Get the arguments from the command line
+file_name = sys.argv[1]  # Bash script file
+branch_name = sys.argv[2]  # Branch name
+repository_name = sys.argv[3]  # Repository name
 
 # Run the bash script using subprocess
 try:
-    result = subprocess.run([bash_script], check=True,
+    result = subprocess.run([file_name, branch_name, repository_name], check=True,
                             text=True, capture_output=True)
 
     # Print the output of the bash script
-    print("Bash script output:")
+    print(f"file_name script output:")
     print(result.stdout)
 
 except subprocess.CalledProcessError as e:
